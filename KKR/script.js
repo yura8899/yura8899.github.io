5df6b4487238db97a0a93d5f6b515dfd26827bf4
yura8899 <51506611+yura8899@users.noreply.github.com>
2019-06-18 13:34:58 +0300

    Add files via upload

---
+++ KKR/script.js
@@ -0,0 +1,9 @@
+menu.onclick=function myFunction(){
+	var x=document.getElementById('topright');
+	if(x.className==="topright"){
+		x.className+="responsive";
+	}
+	else{
+		x.className="topright";
+	}
+}
